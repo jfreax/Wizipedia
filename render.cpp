0cacafa007364772e85daad55b79b0c2f78d4ffc
Jens Dieskau <JDieskau@web.de>
2009-09-29 19:26:56 +0200

    Updated copyright information

--- render.cpp
+++ render.cpp
@@ -1,19 +1,18 @@
 /*
-    <one line to give the program's name and a brief idea of what it does.>
-    Copyright (C) <year>  <name of author>
+Copyright (C) Copyright (C) 2009 Jens Dieskau
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
@@ -331,7 +330,7 @@ bool CRender::Draw()
 	
 	/* Draw index */
 	if ( !index.empty() ) {
-		boxRGBA ( GetWizipedia()->GetScreen(), 20, 10 - position, 150, -positionWIndex + 5, 50, 50, 50, 255 );
+		boxRGBA ( GetWizipedia()->GetScreen(), 20, 10 - position, 150, -positionWIndex + 5, 30, 30, 30, 255 );
 		rect.y = -position;
 		for ( int i = 0; i < index.size(); ++i ) {
 			rect.y += TTF_FontHeight ( GetWizipedia()->GetDefaultFont() );
